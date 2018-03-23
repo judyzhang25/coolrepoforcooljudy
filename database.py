d31b8af80d6acf47b5553808514ee443c55f2640
judyzhang25 <31805219+judyzhang25@users.noreply.github.com>
2018-03-22 23:15:18 -0400

    changed first to avg response time

--- database.py
+++ database.py
@@ -3,6 +3,9 @@ import math
 import string
 import numpy as np
 from bokeh.plotting import figure, output_file, show
+from bokeh.models import DatetimeTickFormatter
+from bokeh.layouts import widgetbox
+from bokeh.models.widgets import Slider
 
 #---------------------LOADING CSV INTO PANDAS DATAFRAME------------------------#
 def load_data(fname):
@@ -29,18 +32,58 @@ def load_data(fname):
 df = load_data('sfpd_dispatch_data_subset.csv')
 #verifying number of columns
 assert(30 == df.shape[1])
-print(df.dtypes)
-print(df.head())
+# print(df.dtypes)
+# print(df.head())
 
-#---------------------RESPONSE TIME VS CALL TIME-------------------------------#
-def response_to_call(df):
-    '''This function graphs the dispatch response time against the time of day
-    the call was made.
+#---------------------AVG RESPONSE TIME VS CALL TIME---------------------------#
+def response_to_call(df, t=5):
+    '''This function graphs the average dispatch response time against the time 
+    of day the call was made.
 
     Input: df (pd.DataFrame)
+           t  (int; the granularity of each time period in min for which an 
+               average response time is calculated)
 
     Output: graph'''
-    pass
+
+    temp = df.set_index('received_timestamp')
+    df_time = temp.index.time
+
+    response_time = []
+    time_of_day = []
+
+    for i,row in df.iterrows():
+        if not pd.isnull(row['on_scene_timestamp']):
+            diff = row['on_scene_timestamp']-row['received_timestamp']
+            diff = diff.total_seconds()//60
+            if diff < 60 and diff > 0:
+                response_time.append(diff)
+                time = df_time[i].replace(minute = df_time[i].minute - (df_time[i].minute%t))
+                time = time.replace(second = 0)
+                time_of_day.append(time)
+
+    assert(len(response_time)==len(time_of_day))
+    response_call_df = pd.DataFrame(data = {'response': response_time, 'call_time': time_of_day})
+    avg_response_time = response_call_df.groupby(['call_time'])['response'].mean()
+
+    TOOLS="hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,save"
+
+    p = figure(plot_width = 800, plot_height = 400, title = "Average Response Time VS Call Time", tools = TOOLS)
+    p.line(avg_response_time.index, avg_response_time)
+    p.xaxis.formatter=DatetimeTickFormatter()
+    p.xaxis.major_label_orientation = math.pi/4
+    p.grid.grid_line_alpha=0.3
+
+    p.xaxis.axis_label = 'Time of Day'
+    p.yaxis.axis_label = 'Average Response'
+
+    slider = Slider(start=0, end=10, value=1, step=.1, title="Stuff")
+
+    show(p)
+    show(slider)
+    return (avg_response_time.index, avg_response_time)
+
+response_to_call(df)
 
 #---------------------ZIPCODE VS CALL TYPE-------------------------------------#
 def calls_per_area(df, zipcode, call='all'):
